6ffdb6c43776a9e65eab02f5668de6827dc616bf
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-19 13:01:43 -0600

    Solved assignment 3

---
+++ src/Assignment3.java
@@ -0,0 +1,22 @@
+public class Assignment3 {
+
+    public static void main(String[] args) {
+        //swap three number using temp variable;
+
+        int temp = 0,
+                firstNum = 100, //200 -- output
+                secondNum = 200, //300 -- output
+                thirdNum = 300; //100 -- output
+
+        System.out.println("firstNum:" + firstNum + " "+ "secondNum:" + secondNum + " "+ "thirdNum:" + thirdNum );
+
+        temp = firstNum;
+        firstNum = secondNum;
+        secondNum = thirdNum;
+        thirdNum = temp;
+
+        System.out.println("------Output after Swapping------");
+        System.out.println("firstNum:" + firstNum + " "+ "secondNum:" + secondNum + " "+ "thirdNum:" + thirdNum );
+
+    }
+}
