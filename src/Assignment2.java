c94fd0d3d7ba25d41110378c675fdcf4f9cad13a
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-19 12:54:39 -0600

    Solved assignment 2

---
+++ src/Assignment2.java
@@ -0,0 +1,17 @@
+public class Assignment2 {
+
+    public static void main(String[] args) {
+        //swap two number without temp
+
+        int firstNum = 300;
+        int secondNum = 100;
+
+        System.out.println("firstNum:"+ firstNum + " " + "secondNum:" + secondNum);
+
+        firstNum = firstNum-200;
+        secondNum = secondNum + 200;
+
+        System.out.println("-----Output after Swapping-----");
+        System.out.println("firstNum:"+ firstNum + " " + "secondNum:" + secondNum);
+    }
+}
