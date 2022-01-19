3ab9861c4986dfd8d92b103e5f93312e83fad89e
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-19 13:18:50 -0600

    Solved assignment 6

---
+++ src/Assignment6.java
@@ -0,0 +1,24 @@
+public class Assignment6 {
+
+    public static void main(String[] args) {
+        //swap four number using temp variable;
+
+        int firstNum = 100, //200 -- output
+                secondNum = 200, //300 -- output
+                thirdNum = 300, //400 -- output
+                fourthNum = 400; //100 -- output
+
+        System.out.println("firstNum:" + firstNum + " "+ "secondNum:" + secondNum + " "+ "thirdNum:" + thirdNum
+                + " "+ "fourthNum:" + fourthNum);
+
+        firstNum +=100;
+        secondNum +=100;
+        thirdNum += 100;
+        fourthNum -= 300;
+
+        System.out.println("------Output after Swapping------");
+        System.out.println("firstNum:" + firstNum + " "+ "secondNum:" + secondNum + " "+ "thirdNum:" + thirdNum
+                + " "+ "fourthNum:" + fourthNum);
+
+    }
+}
