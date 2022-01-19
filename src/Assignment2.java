ff16a828d656b106e4c3e97d6decea57eb6c7f30
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-19 14:40:29 -0600

    made few changes

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -3,13 +3,13 @@ public class Assignment2 {
     public static void main(String[] args) {
         //swap two number without temp
 
-        int firstNum = 300;
+        int firstNum = 200;
         int secondNum = 100;
 
         System.out.println("firstNum:"+ firstNum + " " + "secondNum:" + secondNum);
 
-        firstNum = firstNum-200;
-        secondNum = secondNum + 200;
+        firstNum = firstNum-secondNum;
+        secondNum = secondNum + firstNum;
 
         System.out.println("-----Output after Swapping-----");
         System.out.println("firstNum:"+ firstNum + " " + "secondNum:" + secondNum);
