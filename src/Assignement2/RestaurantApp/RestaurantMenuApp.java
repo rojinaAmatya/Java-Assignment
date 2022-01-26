df69a215a09988b30cd6026c3ebfdd2db39cfb9f
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-26 16:49:28 -0600

    creating restaurant menu app

---
+++ src/Assignement2/RestaurantApp/RestaurantMenuApp.java
@@ -0,0 +1,48 @@
+package Assignement2.RestaurantApp;
+
+import java.util.Scanner;
+
+public class RestaurantMenuApp {
+
+    static void bill(){
+        System.out.println("*********************************************");
+        System.out.println("Biyani Bill : " + BiryaniDetails.biryani_total);
+        System.out.println("*********************************************");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println(" ");
+            System.out.println("********** RESTAURANT MENU **********");
+            System.out.println("   1. Biryani");
+            System.out.println("   2. Drinks");
+            System.out.println("   3. Ice Cream");
+            System.out.println("   4. Bill");
+            System.out.println("   5. Exit");
+            System.out.println("**************************************");
+            System.out.println(" ");
+
+            System.out.println("Enter the choice from the Menu: ");
+            int choice = sc.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    BiryaniDetails details = new BiryaniDetails();
+                    details.biryaniMenu();
+                    break;
+
+                case 4:
+                    bill();
+
+                default:
+                    System.out.println("Please choose between 1 to 5");
+            }
+        }
+
+
+    }
+}
