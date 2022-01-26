57f53bb1293f8c4c54daad48e76318f0b9890e3e
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-26 17:17:37 -0600

    added drink details

---
+++ src/Assignement2/RestaurantApp/DrinksDetails.java
@@ -0,0 +1,18 @@
+package Assignement2.RestaurantApp;
+
+import java.util.Scanner;
+
+public class DrinksDetails {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("*********************************");
+            System.out.println("    1. Soft Drinks           ");
+            System.out.println("    2. Hot Drinks         ");
+            System.out.println("    3. Go back to Main Menu      ");
+            System.out.println("*********************************");
+            System.out.println(" ");
+        }
+    }
+}
