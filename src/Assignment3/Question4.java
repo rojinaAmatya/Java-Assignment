5de01755f69ad742a337d9564bcff63f274e1b8a
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-28 14:07:28 -0600

    Solving array question 4

---
+++ src/Assignment3/Question4.java
@@ -0,0 +1,32 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class Question4 {
+    //Write a Java program to remove a specific element from an array
+    static void removeElement(int arr[], int n){
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == n ){
+                for(int j =i; j < arr.length-1; j++){
+                    arr[j]= arr[j+1];
+                }
+                break;
+            }
+        }
+        System.out.println("All the elements after deletion: ");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(" " + arr[i]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10,24,32,45,65,87,98,25,13,94};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the index you want to remove: ");
+        int index = sc.nextInt();
+        removeElement(arr, index);
+
+    }
+}
