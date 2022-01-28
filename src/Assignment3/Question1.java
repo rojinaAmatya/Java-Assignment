82d0130f76e4053579317cd734410d822d2ee955
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-27 19:28:16 -0600

    Solving array question 1

---
+++ src/Assignment3/Question1.java
@@ -0,0 +1,18 @@
+package Assignment3;
+
+public class Question1 {
+
+    //Write a Java program to calculate the average value of array
+    static void calcAverage(int arr[]){
+        int sum = 0;
+        for(int i: arr){
+            sum += i;}
+        int average = sum/arr.length;
+        System.out.println("The average of given array is: " + average);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60};
+        calcAverage(arr);
+    }
+}
