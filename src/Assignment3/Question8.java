91befebd84b490e8808aadc558f288cd84c87e9e
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-27 20:33:33 -0600

    Solving array question 8

---
+++ src/Assignment3/Question8.java
@@ -0,0 +1,21 @@
+package Assignment3;
+
+public class Question8 {
+    // Write a Java program to find the duplicate values of an array of string values
+    static void duplicateValue(String arr[]){
+        System.out.println("Duplicate String Value are: ");
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i+1; j < arr.length; j++){
+                if(arr[i]== arr[j]){
+                    System.out.print(arr[j]+ " ");
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String arr[] = {"abc", "bdc","xyz","abd","cbc","bdc","oiu","aei","uio","xyx","xyz"};
+        duplicateValue(arr);
+    }
+}
