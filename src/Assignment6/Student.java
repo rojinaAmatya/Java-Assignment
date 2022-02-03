e506d47a2a5fb50853272ed39b9d0681adf31dec
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-03 15:33:14 -0600

    solving assignment 6

---
+++ src/Assignment6/Student.java
@@ -0,0 +1,60 @@
+package Assignment6;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private long mobileNum;
+    private String address;
+    private String course;
+
+    public Student(int studentId, String studentName, long mobileNum, String address, String course) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.mobileNum = mobileNum;
+        this.address = address;
+        this.course = course;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public long getMobileNum() {
+        return mobileNum;
+    }
+
+    public void setMobileNum(long mobileNum) {
+        this.mobileNum = mobileNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+}
+
+
