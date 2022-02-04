30e6b5a6e70262dc66f9e2d67c2fc761dca80534
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-03 19:10:14 -0600

    created account child class for method overriding

--- src/Assignment6/Emp/AccountDepartment.java
+++ src/Assignment6/Emp/AccountDepartment.java
@@ -1,2 +1,16 @@
-package Assignment6.Emp;public class AccountDepartment {
+package Assignment6.Emp;
+
+import java.util.Scanner;
+
+class AccountDepartment extends Employee{
+
+    public AccountDepartment(int empNum, String empName, String depName) {
+        super(empNum, empName, depName);
+    }
+
+
+    @Override
+    int CalculateSalary() {
+        return super.CalculateSalary()*7;
+    }
 }
