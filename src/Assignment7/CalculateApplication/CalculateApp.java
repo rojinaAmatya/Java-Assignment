069197ccfc7cfbfa2372727a10be72fbff9e6aaf
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-04 17:50:14 -0600

    interface practice

---
+++ src/Assignment7/CalculateApplication/CalculateApp.java
@@ -0,0 +1,7 @@
+package Assignment7.CalculateApplication;
+
+public interface CalculateApp {
+    public void add(int x, int y);
+    public void sub(int x, int y);
+    public void mul(int x, int y);
+}
