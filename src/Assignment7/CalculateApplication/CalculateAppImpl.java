0c10258955911e78df3e3766b62a8bee5fae6a19
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-04 17:55:14 -0600

    Cretaing interface implementation methods

---
+++ src/Assignment7/CalculateApplication/CalculateAppImpl.java
@@ -0,0 +1,23 @@
+package Assignment7.CalculateApplication;
+
+public class CalculateAppImpl implements CalculateApp {
+    @Override
+    public void add(int x, int y) {
+        int addition = x+ y;
+        System.out.println("The addition of two number is: " + addition );
+    }
+
+    @Override
+    public void sub(int x, int y) {
+        int subtraction = x - y;
+        System.out.println("The addition of two number is: " + subtraction );
+
+    }
+
+    @Override
+    public void mul(int x, int y) {
+        int multiplication = x * y;
+        System.out.println("The addition of two number is: " + multiplication );
+
+    }
+}
